afdf0f207634d4b78df5de87da823f9078e08e74
bryan <bryanleehartwell@gmail.com>
2025-11-05 21:51:45 -0500

    Completed lesson 9

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 import pygame
 from constants import *
+from player import Player
+from logger import log_state
 
 def main():
-#    print("Starting Asteroids!")
-#    print(f"Screen width: {SCREEN_WIDTH}")
-#    print(f"Screen height: {SCREEN_HEIGHT}")
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock=pygame.time.Clock()
     dt=0
+    player=Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        log_state()
         screen.fill((0,0,0))
+        player.draw(screen)
         dt=clock.tick(60)/1000
         pygame.display.flip()
 
